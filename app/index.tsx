ee47b65f778a70ac143b9121f4f4b30dccc478e4
lisperey <pilrss16@gmail.com>
2024-05-16 15:58:27 -0300

    feat: Construindo barra de navegação

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,35 @@
-import { Image, StyleSheet } from 'react-native';
-import { View, Text, Link } from 'react-native';
+import { Image, StyleSheet, View, Text, ScrollView  } from 'react-native';
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../constants'
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <Link href="/home">Go to home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-start items-center h-full px-4">
+          <Image
+            source={images.logo} 
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards} 
+            className="max-w--[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Descubra Infinitas Possibilidades com {''}
+              <Text className="text-secondary-200">Aora</Text> 
+            </Text>
+
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
